c78d3372eecfd14445d9037ba0912e2a62442c84
leeneo@xingzhihen.com <leeneo@xingzhihen.com>
2018-07-06 14:29:13 +0800

    11

---
+++ ChangeImg/ChangeImg/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace ChangeImg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.txtImgPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSavePath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtWith = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(246, 104);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 0;
+            this.btnSubmit.Text = "提交";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // txtImgPath
+            // 
+            this.txtImgPath.Location = new System.Drawing.Point(83, 27);
+            this.txtImgPath.Name = "txtImgPath";
+            this.txtImgPath.Size = new System.Drawing.Size(238, 21);
+            this.txtImgPath.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "图片路径：";
+            // 
+            // txtSavePath
+            // 
+            this.txtSavePath.Location = new System.Drawing.Point(83, 63);
+            this.txtSavePath.Name = "txtSavePath";
+            this.txtSavePath.Size = new System.Drawing.Size(238, 21);
+            this.txtSavePath.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "保存路径：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "图片大小：";
+            // 
+            // txtWith
+            // 
+            this.txtWith.Location = new System.Drawing.Point(105, 105);
+            this.txtWith.Name = "txtWith";
+            this.txtWith.Size = new System.Drawing.Size(47, 21);
+            this.txtWith.TabIndex = 1;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.ForeColor = System.Drawing.Color.Red;
+            this.lblResult.Location = new System.Drawing.Point(32, 157);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 12);
+            this.lblResult.TabIndex = 3;
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(191, 105);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(47, 21);
+            this.txtHeight.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(83, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 12);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "宽";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(170, 110);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 12);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "高";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 194);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.txtWith);
+            this.Controls.Add(this.txtSavePath);
+            this.Controls.Add(this.txtImgPath);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSubmit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TextBox txtImgPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSavePath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtWith;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ ChangeImg/ChangeImg/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChangeImg
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string path = txtImgPath.Text.Trim();
+            string savepath = txtSavePath.Text.Trim();
+            int num = 0;
+            if (!System.IO.Directory.Exists(path))
+            {
+                return;
+            }
+            if (!System.IO.Directory.Exists(savepath))
+            {
+                System.IO.Directory.CreateDirectory(savepath);
+            }
+            string[] fileStrs = System.IO.Directory.GetFiles(path);
+            foreach (string s in fileStrs)
+            {
+                System.IO.FileInfo info = new System.IO.FileInfo(s);
+                if (info.Extension == ".png" || info.Extension == ".jpg")
+                {
+                    Image img = GetFile(s);
+                    Bitmap bit = GetNewSizeBitmap(img);
+                    SaveImage(bit, info.Name, info.Extension, savepath);
+                }
+                ++num;
+            }
+            lblResult.Text= "共处理：" + num + " 张图片";
+        }
+        public Image GetFile(string path)
+        {
+            FileStream stream = File.OpenRead(path);
+            int fileLength = 0;
+            fileLength = (int)stream.Length;
+            Byte[] image = new Byte[fileLength];
+            stream.Read(image, 0, fileLength);
+            System.Drawing.Image result = System.Drawing.Image.FromStream(stream);
+            stream.Close();
+            return result;
+        }
+        public Bitmap GetNewSizeBitmap(Image img)
+        {
+            int newWidth = Convert.ToInt32(txtWith.Text.Trim());
+            int newHeight = Convert.ToInt32(txtHeight.Text.Trim());
+            Size s = new Size(newWidth, newHeight);
+            Bitmap newBit = new Bitmap(img, s);
+            return newBit;
+        }
+        public void SaveImage(Bitmap bit, string name, string ext, string savepath)
+        {
+            bit.Save(savepath + "/" + name);
+            bit.Dispose();
+        }
+    }
+}
