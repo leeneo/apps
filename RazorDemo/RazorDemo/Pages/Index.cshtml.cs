a8b29d6ad85e1280ed7c9f187192a765cea940a6
leeneo@xingzhihen.com <leeneo@xingzhihen.com>
2018-11-27 10:19:57 +0800

    1

---
+++ RazorDemo/RazorDemo/Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [System.ComponentModel.DataAnnotations.RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [System.ComponentModel.DataAnnotations.Required]
+        [System.ComponentModel.DataAnnotations.StringLength(30)]
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "TEst";
+        }
+    }
+}
